b784573201c15c168f3407945df885c43ac5d1bc
JustArist <git@justarist.cc>
2026-07-16 21:34:18 +0300

    Fix BottomMenu

--- app/src/main/java/cc/rccstudios/map/ui/screens/BottomMenu.kt
+++ app/src/main/java/cc/rccstudios/map/ui/screens/BottomMenu.kt
@@ -1,7 +1,6 @@
 package cc.rccstudios.map.ui.screens
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -9,29 +8,24 @@ import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Map
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import cc.rccstudios.map.R
 import cc.rccstudios.map.ui.MainModelView
-import cc.rccstudios.map.ui.theme.RCCMapTheme
 
 enum class Destination(
     val route: String,
@@ -77,7 +71,7 @@ fun AppNavHost(
                         viewModel = viewModel,
                         modifier = modifier
                     )
-                    Destination.HOME -> SettingsScreen(
+                    Destination.HOME -> RegisterScreen(
                         viewModel = viewModel,
                         modifier = modifier
                     )
@@ -97,19 +91,27 @@ fun BottomMenu(
     modifier: Modifier = Modifier
 ) {
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
     val startDestination = Destination.HOME
-    var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
+    val currentDestination = navBackStackEntry?.destination
 
     Scaffold(
         modifier = modifier,
         bottomBar = {
             NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
                 Destination.entries.forEachIndexed { index, destination ->
+                    val isSelected = currentDestination?.hierarchy?.any { it.route == destination.route } == true
+
                     NavigationBarItem(
-                        selected = selectedDestination == index,
+                        selected = isSelected,
                         onClick = {
-                            navController.navigate(route = destination.route)
-                            selectedDestination = index
+                            navController.navigate(destination.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
                         },
                         icon = {
                             Icon(
@@ -124,41 +126,5 @@ fun BottomMenu(
         }
     ) { contentPadding ->
         AppNavHost(viewModel, navController, startDestination, modifier = Modifier.padding(contentPadding))
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun BottomMenuPreview() {
-    RCCMapTheme{
-        val navController = rememberNavController()
-        val startDestination = Destination.HOME
-        var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
-
-        Scaffold(
-            modifier = Modifier,
-            bottomBar = {
-                NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
-                    Destination.entries.forEachIndexed { index, destination ->
-                        NavigationBarItem(
-                            selected = selectedDestination == index,
-                            onClick = {
-                                navController.navigate(route = destination.route)
-                                selectedDestination = index
-                            },
-                            icon = {
-                                Icon(
-                                    destination.icon,
-                                    contentDescription = stringResource(destination.contentDescription)
-                                )
-                            },
-                            label = { Text(stringResource(destination.label)) }
-                        )
-                    }
-                }
-            }
-        ) {
-
-        }
     }
 }
