f4d4cad1f4d260fd003c735df8f6451ccb3c21e9
JustArist <git@justarist.cc>
2026-08-25 20:08:36 +0000

    fix: remove deprecated stuff

--- app/src/main/java/cc/rccstudios/map/ui/screens/settings/Components.kt
+++ app/src/main/java/cc/rccstudios/map/ui/screens/settings/Components.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
@@ -554,7 +555,12 @@ fun SettingSoundSelector(
                 onValueChange = {},
                 readOnly = true,
                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                modifier = Modifier.fillMaxWidth().menuAnchor(),
+                modifier = Modifier
+                    .menuAnchor(
+                        type = ExposedDropdownMenuAnchorType.PrimaryNotEditable,
+                        enabled = enabled
+                    )
+                    .fillMaxWidth(),
                 enabled = enabled,
                 shape = RoundedCornerShape(8.dp)
             )
