f8205914a43cf26cc0f43371a85415950518d20d
JustArist <git@justarist.cc>
2026-08-27 19:47:58 +0000

    feat: update theme to RCC Map brand colors

--- app/src/main/java/cc/rccstudios/map/ui/theme/Color.kt
+++ app/src/main/java/cc/rccstudios/map/ui/theme/Color.kt
@@ -2,10 +2,32 @@ package cc.rccstudios.map.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BrandGreen = Color(0xFF008766)
+val OnBrandGreen = Color(0xFFFFFFFF)
+val BrandGreenContainer = Color(0xFFD7F4ED)
+val OnBrandGreenContainer = Color(0xFF004736)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BrandSecondary = Color(0xFF40645B)
+val OnBrandSecondary = Color(0xFFFFFFFF)
+val BrandSecondaryContainer = Color(0xFFD8E9E5)
+val OnBrandSecondaryContainer = Color(0xFF21312D)
+
+val BrandTertiary = Color(0xFF49713D)
+val OnBrandTertiary = Color(0xFFFFFFFF)
+val BrandTertiaryContainer = Color(0xFFDBEBD6)
+val OnBrandTertiaryContainer = Color(0xFF23341D)
+
+val BrandGreenDark = Color(0xFF90DFCC)
+val OnBrandGreenDark = Color(0xFF00523E)
+val BrandGreenContainerDark = Color(0xFF008564)
+val OnBrandGreenContainerDark = Color(0xFFDBF0EB)
+
+val BrandSecondaryDark = Color(0xFFC2D6D1)
+val OnBrandSecondaryDark = Color(0xFF253733)
+val BrandSecondaryContainerDark = Color(0xFF3B544E)
+val OnBrandSecondaryContainerDark = Color(0xFFDAE7E4)
+
+val BrandTertiaryDark = Color(0xFFC5DABE)
+val OnBrandTertiaryDark = Color(0xFF273B21)
+val BrandTertiaryContainerDark = Color(0xFF3C5D32)
+val OnBrandTertiaryContainerDark = Color(0xFFDCE9D8)
